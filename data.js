736080c3947d89c3a9c3d2ea0fa01b6788ebe5f1
JulShelbyMx <thomasjuleseliot@gmail.com>
2026-06-28 19:25:43 +0200

    jake s4 ep 4

--- data.js
+++ data.js
@@ -816,6 +816,7 @@ const DATA = {
       {num:100,title:'GTA 5 RP À ZÉRO ! #1 - Saison 4 (Jake Winters : Je vous présente mes enfants)',videoId:'0vZnfLlhKZo'},
       {num:101,title:'GTA 5 RP À ZÉRO ! #2 - (J\'ai fait une énorme connerie avec mon ex copine…)',videoId:'NukqTJn8xPU'},
       {num:102,title:'GTA 5 RP À ZÉRO ! #3 - (Mon ex m\'annonce que j\'ai un fils caché !)',videoId:'bN0YeA0yJk8'},
+      {num:103,title:'GTA 5 RP À ZÉRO ! #4 - (Je prépare la plus grosse évasion !)',videoId:'ghROG6l29_w', youtubeLink:true},
 // youtubeLink:true
      ],
 }, },
@@ -1457,12 +1458,12 @@ const DATA = {
     episode: {
       active: true,
       label: 'NOUVEL ÉPISODE',
-      text: '(Jake Winters) : GTA 5 RP À ZÉRO ! #3 - (Mon ex m\'annonce que j\'ai un fils caché !)',
+      text: '(Jake Winters) : GTA 5 RP À ZÉRO ! #4 - (Je prépare la plus grosse évasion !)',
       link: {
         familyId: 'winters',
         charId:   'jake-winters',
         season:   'Saison 4',
-        epNum:    102
+        epNum:    103
       },
       
     },
