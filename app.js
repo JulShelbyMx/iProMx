a9d0f9da106d69b502bcbd4c436f08c805d69caf
JulShelbyMx <thomasjuleseliot@gmail.com>
2026-07-01 19:26:36 +0200

    images 160=>326 galerie

--- app.js
+++ app.js
@@ -242,7 +242,7 @@ function appInit(cb) {
 
     await new Promise(res => {
       const s = document.createElement('script');
-      s.src = '/firebase-auth.js?v=46'; s.onload = res; s.onerror = res;
+      s.src = '/firebase-auth.js?v=47'; s.onload = res; s.onerror = res;
       document.body.appendChild(s);
     });
 
